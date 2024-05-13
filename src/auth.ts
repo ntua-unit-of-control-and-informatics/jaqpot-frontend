461bcd6b71e72fe6925610431f8e3952c7ac3689
alarv <alarvfm@gmail.com>
2024-05-13 13:37:05 +0300

    fix: add access token for server-side requests

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,17 @@
-import NextAuth from "next-auth"
-import KeycloakProvider from "next-auth/providers/keycloak";
+import NextAuth from 'next-auth';
+import KeycloakProvider from 'next-auth/providers/keycloak';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    providers: [KeycloakProvider({
-        clientId: process.env.KEYCLOAK_CLIENT_ID!,
-        clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
-        issuer: process.env.KEYCLOAK_ISSUER
-    })],
-})
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER,
+    }),
+  ],
+  callbacks: {
+    session({ session, token }) {
+      return { ...session, accessToken: token.accessToken };
+    },
+  },
+});
