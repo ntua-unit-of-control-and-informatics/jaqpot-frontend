76032b95d341ed60cf9e5d9dabfb945c8f2ed397
alarv <alarvfm@gmail.com>
2024-05-13 11:20:51 +0300

    fix: migrate to next-auth 5

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    providers: [KeycloakProvider({
+        clientId: process.env.KEYCLOAK_CLIENT_ID!,
+        clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+        issuer: process.env.KEYCLOAK_ISSUER
+    })],
+})
