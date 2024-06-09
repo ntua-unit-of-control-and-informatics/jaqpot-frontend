bcc347fed3395073aea92f0726b3a7ef1abb348d
alarv <alarvfm@gmail.com>
2024-06-09 21:09:58 +0300

    feat: add tables for FeaturesTab.tsx

--- src/app/dashboard/models/[modelId]/components/tabs/FeaturesTab.tsx
+++ src/app/dashboard/models/[modelId]/components/tabs/FeaturesTab.tsx
@@ -1,5 +1,13 @@
 import { ModelDto } from '@/app/api.types';
 import { Card, CardBody } from '@nextui-org/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/table';
 
 interface FeaturesTabProps {
   model: ModelDto;
@@ -7,41 +15,45 @@ interface FeaturesTabProps {
 
 export default function FeaturesTab({ model }: FeaturesTabProps) {
   return (
-    <div className="flex divide-dashed divide-x">
+    <div className="grid grid-cols-1 md:grid-cols-2 divide-dashed divide-x">
       <div className="p-4">
         <h2 className="font-semibold mb-4">Independent Features</h2>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
-          {model.independentFeatures.map((independentFeature, index) => (
-            <Card
-              className="p-5 text-sm min-h-36 max-w-72"
-              shadow="sm"
-              key={index}
-            >
-              <CardBody className="overflow-visible p-0">
-                <p>Name: {independentFeature.name}</p>
-                <p>Type: {independentFeature.featureType}</p>
-              </CardBody>
-            </Card>
-          ))}
-        </div>
+        <Table aria-label="Independent features table">
+          <TableHeader>
+            <TableColumn>Name</TableColumn>
+            <TableColumn>Description</TableColumn>
+            <TableColumn>Type</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {model.independentFeatures.map((independentFeature, index) => (
+              <TableRow key={index}>
+                <TableCell>{independentFeature.name}</TableCell>
+                <TableCell>{independentFeature.description}</TableCell>
+                <TableCell>{independentFeature.featureType}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
       <div className="p-4">
         <h2 className="mb-4 font-semibold ">Dependent Features</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
-          {model.dependentFeatures.map((dependentFeature, index) => (
-            <Card
-              className="p-5 text-sm  min-h-36 max-w-72"
-              shadow="sm"
-              key={index}
-            >
-              <CardBody className="overflow-visible p-0">
-                <p>Name: {dependentFeature.name}</p>
-                <p>Type: {dependentFeature.featureType}</p>
-              </CardBody>
-            </Card>
-          ))}
-        </div>
+        <Table aria-label="Dependent features table">
+          <TableHeader>
+            <TableColumn>Name</TableColumn>
+            <TableColumn>Description</TableColumn>
+            <TableColumn>Type</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {model.dependentFeatures.map((dependentFeature, index) => (
+              <TableRow key={index}>
+                <TableCell>{dependentFeature.name}</TableCell>
+                <TableCell>{dependentFeature.description}</TableCell>
+                <TableCell>{dependentFeature.featureType}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
