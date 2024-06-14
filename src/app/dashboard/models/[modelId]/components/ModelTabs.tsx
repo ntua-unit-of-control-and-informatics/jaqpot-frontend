632962f49fa3c5f516c9c966b5e9e809e43d7cfc
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-06-14 17:07:10 +0300

    fix: dark transparent background model tabs

--- src/app/dashboard/models/[modelId]/components/ModelTabs.tsx
+++ src/app/dashboard/models/[modelId]/components/ModelTabs.tsx
@@ -25,10 +25,10 @@ export default function ModelTabs({ model }: ModelTabsProps) {
         tabList:
           'gap-8 w-full bg-transparent dark:bg-transparent rounded-none border-b-1 p-0 w-full',
         cursor:
-          'w-full rounded-none bg-transparent group-data-[selected=true]:border-b-2 group-data-[selected=true]:border-indigo-800 dark:group-data-[selected=true]:border-b-2 dark:group-data-[selected=true]:border-white shadow-none dark:text-white',
+          'w-full rounded-none bg-transparent dark:bg-transparent group-data-[selected=true]:border-b-2 group-data-[selected=true]:border-indigo-800 dark:group-data-[selected=true]:border-b-2 dark:group-data-[selected=true]:border-white shadow-none dark:text-white',
         tab: 'max-w-fit px-0 h-12 rounded-none border-none box-shadow-none',
         tabContent:
-          'rounded-none border-none box-shadow-none group-data-[selected=true]:text-indigo-600',
+          'rounded-none border-none box-shadow-none group-data-[selected=true]:text-indigo-600 dark:group-data-[selected=true]:text-white',
       }}
     >
       <Tab key="description" title="Description">
