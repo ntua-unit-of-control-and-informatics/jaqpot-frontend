dda053d589f63b413ee6191a6cd12827458d20f5
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-07-24 15:21:10 +0300

    fix: minor ui fixes

--- src/app/dashboard/organizations/[orgName]/components/OrganizationTabs.tsx
+++ src/app/dashboard/organizations/[orgName]/components/OrganizationTabs.tsx
@@ -31,7 +31,7 @@ export default function OrganizationTabs({
       <Tab key="description" title="Description">
         <MarkdownRenderer>{organization.description}</MarkdownRenderer>
       </Tab>
-      <Tab key="models" title="Associated models">
+      <Tab key="models" title="Models">
         <OrganizationAssociatedModelsTab organization={organization} />
       </Tab>
       {organization.canEdit && (
