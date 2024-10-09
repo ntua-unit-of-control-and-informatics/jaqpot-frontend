833a5e1f62b359950dc9f5f57bc3f09675833fe2
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-09 16:08:34 +0300

    fix: justify homepage text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,7 +33,7 @@ export default function Dashboard() {
             className="relative left-[calc(20%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-2 sm:py-6">
+        <div className="mx-auto max-w-3xl py-2 sm:py-6">
           {/*<div className="hidden sm:mb-8 sm:flex sm:justify-center">*/}
           {/*  <div className="relative rounded-full px-3 py-1 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">*/}
           {/*    Announcing our next round of funding.{' '}*/}
