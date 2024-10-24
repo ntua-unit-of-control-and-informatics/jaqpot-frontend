5fdf615b59c0bdf84449f97d9df65050456a97d7
Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>
2024-10-24 16:35:16 +0300

    feat(JAQPOT-338): create charts for pbpk models (#67)
    * feat(JAQPOT-338): create charts for pbpk models
    
    * fix: improve colors

--- src/app/dashboard/models/[modelId]/components/DatasetResults.tsx
+++ src/app/dashboard/models/[modelId]/components/DatasetResults.tsx
@@ -34,6 +34,8 @@ import { getKeyValue, useDisclosure } from '@nextui-org/react';
 import DoaTableCell from '@/app/dashboard/models/[modelId]/components/DoaTableCell';
 import FeatureEditModal from '@/app/dashboard/models/[modelId]/components/FeatureEditModal';
 import DoaModal from '@/app/dashboard/models/[modelId]/components/DoaModal';
+import { Tab, Tabs } from '@nextui-org/tabs';
+import PBPKPlots from '@/app/dashboard/models/[modelId]/components/PBPKPlots';
 
 const log = logger.child({ module: 'dataset' });
 
@@ -136,107 +138,117 @@ export default function DatasetResults({
   const tableRows = resultTableData.rows;
 
   return (
-    <div className="mb-20 mt-5 flex flex-col gap-4">
-      {dataset?.status === 'FAILURE' && model.isAdmin && (
-        <div className="max-w-xl">
-          <Accordion>
-            <AccordionItem
-              key="1"
-              aria-label="Accordion 1"
-              subtitle="You can only see this if you are a nerd"
-              title="Data for nerds"
-              startContent={<BugAntIcon className="size-6" />}
-            >
-              <p className="text-sm">
-                legacy prediction service: {model.legacyPredictionService}
-              </p>
-              <p className="text-sm">
-                Failure reason: {dataset?.failureReason}
-              </p>
-            </AccordionItem>
-          </Accordion>
-        </div>
-      )}
-      <h2 className="text-2xl font-bold leading-7 sm:text-3xl sm:tracking-tight">
-        Result
-      </h2>
-      <div>
-        <Link
-          href={`/dashboard/models/${model.id}/results/${datasetId}`}
-          isExternal
-          showAnchorIcon
-          className="mr-2"
-        >
-          ID {datasetId}
-        </Link>
-        {getDatasetStatusNode(dataset)}
-      </div>
-      {!isLoaded && (
-        <div className="flex w-full flex-col gap-2">
-          <Skeleton className="h-3 w-3/5 rounded-lg" />
-          <Skeleton className="h-3 w-4/5 rounded-lg" />
-          <Skeleton className="h-3 w-2/5 rounded-lg" />
-          <Skeleton className="w-5/5 h-3 rounded-lg" />
-        </div>
-      )}
-      {isLoaded && dataset?.status === 'SUCCESS' && (
-        <>
-          <div>
-            <Button
-              color="primary"
-              startContent={<ArrowDownTrayIcon className="size-6" />}
-              className="my-2"
-              onPress={() => downloadResultsCSV(model)}
-            >
-              Export CSV
-            </Button>
+    <div className="mb-20 mt-5">
+      <Tabs>
+        <Tab key="result" title="Result">
+          <div className="flex flex-col gap-4">
+            {dataset?.status === 'FAILURE' && model.isAdmin && (
+              <div className="max-w-xl">
+                <Accordion>
+                  <AccordionItem
+                    key="1"
+                    aria-label="Accordion 1"
+                    subtitle="You can only see this if you are a nerd"
+                    title="Data for nerds"
+                    startContent={<BugAntIcon className="size-6" />}
+                  >
+                    <p className="text-sm">
+                      legacy prediction service: {model.legacyPredictionService}
+                    </p>
+                    <p className="text-sm">
+                      Failure reason: {dataset?.failureReason}
+                    </p>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+            )}
+            <div>
+              <Link
+                href={`/dashboard/models/${model.id}/results/${datasetId}`}
+                isExternal
+                showAnchorIcon
+                className="mr-2"
+              >
+                ID {datasetId}
+              </Link>
+              {getDatasetStatusNode(dataset)}
+            </div>
+            {!isLoaded && (
+              <div className="flex w-full flex-col gap-2">
+                <Skeleton className="h-3 w-3/5 rounded-lg" />
+                <Skeleton className="h-3 w-4/5 rounded-lg" />
+                <Skeleton className="h-3 w-2/5 rounded-lg" />
+                <Skeleton className="w-5/5 h-3 rounded-lg" />
+              </div>
+            )}
+            {isLoaded && dataset?.status === 'SUCCESS' && (
+              <>
+                <div>
+                  <Button
+                    color="primary"
+                    startContent={<ArrowDownTrayIcon className="size-6" />}
+                    className="my-2"
+                    onPress={() => downloadResultsCSV(model)}
+                  >
+                    Export CSV
+                  </Button>
+                </div>
+                <Table aria-label="Prediction table" className="mb-6">
+                  <TableHeader columns={tableHeaders}>
+                    {(column) => (
+                      <TableColumn key={column.key}>{column.label}</TableColumn>
+                    )}
+                  </TableHeader>
+
+                  <TableBody
+                    items={tableRows}
+                    loadingState={loadingState}
+                    emptyContent={'No results to display.'}
+                  >
+                    {(item) => (
+                      <TableRow key={item.key}>
+                        {(columnKey) => {
+                          if (columnKey === 'doa') {
+                            return (
+                              <TableCell>
+                                <DoaTableCell
+                                  value={getKeyValue(item, columnKey)}
+                                  onPress={() => {
+                                    setSelectedRow(item);
+                                    onDoaModalOpen();
+                                  }}
+                                />
+                              </TableCell>
+                            );
+                          }
+                          return (
+                            <TableCell>
+                              {getKeyValue(item, columnKey)}
+                            </TableCell>
+                          );
+                        }}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </>
+            )}
+
+            {selectedRow && (
+              <DoaModal
+                doaDetails={selectedRow.doaDetails}
+                isOpen={isDoaModalOpen}
+                onOpenChange={onDoaModalChange}
+              />
+            )}
           </div>
-          <Table aria-label="Prediction table" className="mb-6">
-            <TableHeader columns={tableHeaders}>
-              {(column) => (
-                <TableColumn key={column.key}>{column.label}</TableColumn>
-              )}
-            </TableHeader>
-
-            <TableBody
-              items={tableRows}
-              loadingState={loadingState}
-              emptyContent={'No results to display.'}
-            >
-              {(item) => (
-                <TableRow key={item.key}>
-                  {(columnKey) => {
-                    if (columnKey === 'doa') {
-                      return (
-                        <TableCell>
-                          <DoaTableCell
-                            value={getKeyValue(item, columnKey)}
-                            onPress={() => {
-                              setSelectedRow(item);
-                              onDoaModalOpen();
-                            }}
-                          />
-                        </TableCell>
-                      );
-                    }
-                    return (
-                      <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                    );
-                  }}
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </>
-      )}
-
-      {selectedRow && (
-        <DoaModal
-          doaDetails={selectedRow.doaDetails}
-          isOpen={isDoaModalOpen}
-          onOpenChange={onDoaModalChange}
-        />
-      )}
+        </Tab>
+        {model.type === 'R_PBPK' && dataset && dataset.status === 'SUCCESS' && (
+          <Tab key="plots" title="Plots">
+            <PBPKPlots model={model} dataset={dataset} />
+          </Tab>
+        )}
+      </Tabs>
     </div>
   );
 }
