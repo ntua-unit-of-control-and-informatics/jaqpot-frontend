61c34c4f25b39dbd69546b71a20a4b78dfb7c915
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-06-12 11:09:54 +0300

    fix: fix next.config.mjs server external packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.dicebear.com',
-                port: '',
-                pathname: '/9.x/**',
-            },
-        ],
-    },
-    experimental: {
-        serverComponentsExternalPackages: ["pino", "pino-pretty"],
-    },
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/9.x/**',
+      },
+    ],
+  },
+  serverExternalPackages: ['pino', 'pino-pretty'],
 };
 
 export default nextConfig;
