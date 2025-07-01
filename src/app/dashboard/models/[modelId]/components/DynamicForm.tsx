3f5bfd1873a04dfc0d650237569c2a7dcd5c008c
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-07-01 12:27:24 +0300

    fix: remove tabindex from label

--- src/app/dashboard/models/[modelId]/components/DynamicForm.tsx
+++ src/app/dashboard/models/[modelId]/components/DynamicForm.tsx
@@ -161,7 +161,7 @@ function generateFieldLabel(field: DynamicFormField) {
   return (
     <>
       {field.labelTooltip && (
-        <Tooltip content={field.labelTooltip} closeDelay={0}>
+        <Tooltip content={field.labelTooltip} closeDelay={0} tabIndex={-1}>
           <InformationCircleIcon className="mr-0.5 size-4 flex-shrink-0 text-gray-400" />
         </Tooltip>
       )}
